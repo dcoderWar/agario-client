dd9ac557be8f07f4064a39b76a71e3da36bed402
Darrius <dcoderWar@gmail.com>
2015-11-03 21:08:58 -0600

    update

--- agar.io/config/index.js
+++ agar.io/config/index.js
@@ -1,6 +1,39 @@
 'use strict';
 
-const config = module.exports = require('./config.json');
+const config = module.exports = (function validateConfig() {
+    let config, missing = [], example = {},
+        required = ['client', 'bot', 'helper', 'eventLog'];
+
+    try {
+        config = require('./config.json');
+    }
+    catch (error) {
+        error.name = 'ConfigError';
+        error.message += '\nConfigError: Cannot load configuration from: ' +
+            __dirname + require('path').sep + 'config.json\n';
+
+        console.error(error.stack + '\n');
+        process.exit(1);
+    }
+
+    required.forEach(key => {
+        if (!config.hasOwnProperty(key) || !config[key].hasOwnProperty('options'))
+            missing.push(key);
+    });
+
+    if (missing.length) {
+        missing.forEach(key => example[key] = {options: {}});
+
+        let error = new Error('config.json is missing the following' +
+            ' required properties:\n\n' + JSON.stringify(example, null, '\t') + '\n');
+        error.name = 'ConfigError';
+
+        console.error(error.stack + '\n');
+        process.exit(1);
+    }
+
+    return config;
+}());
 
 module.exports.defineOptions = defineOptions;
 module.exports.createOptions = createOptions;
